040df303e35efd24323d84ff32d00bb94ed23448
Yohei Ohashi (Work) <ohashi-yohei@fujibuturyu.co.jp>
2025-11-13 17:42:16 +0900

    feat: no_oop_pokemonの作成中

--- youtube/oop-learning/no_oop_pokemon.py
+++ youtube/oop-learning/no_oop_pokemon.py
@@ -1,10 +1,27 @@
+def appearance(my_pokemon, enemy_pokemon):
+    print(f"{my_pokemon["name"]}があらわれた。{my_pokemon["name"]}のHPは{my_pokemon["hp"]}だ。")
+    print(f"{enemy_pokemon["name"]}があらわれた。{enemy_pokemon["name"]}のHPは{enemy_pokemon["hp"]}だ。")
+
+def calc_hp(atk_pokemon, def_pokemon):
+    def_hp = def_pokemon["hp"]
+    
+    if def_hp - atk_pokemon["atk"][0][1] > 0:
+        def_hp -= atk_pokemon["atk"][0][1]
+        return def_hp 
+    else:
+        return 0
+    
+
+def battle(my_pokemon, enemy_pokemon):
+    print(f"{my_pokemon["name"]}のこうげき！{my_pokemon["atk"][0][0]}！{enemy_pokemon["name"]}は{my_pokemon["atk"][0][1]}ダメージをもらった。{enemy_pokemon["name"]}のHPは{calc_hp(my_pokemon, enemy_pokemon)}だ。")
+
 def main():
     pikachu = {
         "name": "ピカチュウ",
         "hp": 20,
-        "atk": {
-            "10万ボルト": 10
-        }
+        "atk": [
+            ["10万ボルト", 10]
+        ]
     }
     hitokage = {
         "name": "ヒトカゲ",
@@ -14,8 +31,8 @@ def main():
         }
     }
     
-    
-    print("TEST")
+    appearance(pikachu, hitokage)
+    battle(pikachu, hitokage)
 
 if __name__ in "__main__":
     main()
