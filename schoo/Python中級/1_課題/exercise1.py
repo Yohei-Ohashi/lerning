a153c0335991a2da80ce8dbefd3ae54943db3b9b
Yohei Ohashi <hashi.life@outlook.jp>
2025-11-21 06:30:43 +0900

    feat [中級] #1課題: 取得したシートを一つのExcelファイルにして保存する処理を実装

--- schoo/Python中級/1_課題/exercise1.py
+++ schoo/Python中級/1_課題/exercise1.py
@@ -18,7 +18,10 @@ from pathlib import Path
 
 # 定数定義
 SHEET_NAME = "202203"
-DIR_PATH = "schoo/Python中級/1_課題/input"
+INPUT_DIR_PATH = "schoo/Python中級/1_課題/input"
+OUTPUT_DIR_PATH = "schoo/Python中級/1_課題/output"
+OUTPUT_FILE_NAME = "output_file.xlsx"
+OUTPUT_FILE_PATH = Path(OUTPUT_DIR_PATH) / OUTPUT_FILE_NAME
 
 
 def get_file_list(dir_path) -> list[str]:
@@ -31,6 +34,7 @@ def get_file_list(dir_path) -> list[str]:
     ]
     # 一応拡張子がxlsxだけに絞る
     xlsx_file_list = [xf for xf in file_list if xf.endswith(".xlsx") == True]
+
     return xlsx_file_list
 
 
@@ -47,20 +51,30 @@ def make_dfs(dir_path, xlsx_file_list):
     return df_list
 
 
+def output_excel(df_list, xlsx_file_list, output_file_path):
+    """取得したシートを一つのExcelファイルにして保存"""
+
+    output_file_path.parent.mkdir(parents=True, exist_ok=True)
+
+    with pd.ExcelWriter(output_file_path) as writer:
+        for i, df in enumerate(df_list):
+            sheet_name = xlsx_file_list[i].replace(".xlsx", "")
+            df.to_excel(writer, sheet_name=sheet_name)
+
+
 # メイン処理
 def main():
     # フォルダの中にあるExcelファイル一覧を取得
-    xlsx_file_list = get_file_list(DIR_PATH)
+    xlsx_file_list = get_file_list(INPUT_DIR_PATH)
     print(xlsx_file_list)
 
     # 各Excelファイル内の特定シートのみ取得
-    df_list = make_dfs(DIR_PATH, xlsx_file_list)
+    df_list = make_dfs(INPUT_DIR_PATH, xlsx_file_list)
     print(len(df_list))
+
+    # 取得したシートを一つのExcelファイルにして保存
+    output_excel(df_list, xlsx_file_list, OUTPUT_FILE_PATH)
 
 
 if __name__ == "__main__":
     main()
-
-
-# TODO
-# 取得したシートを一つのExcelファイルにして保存
