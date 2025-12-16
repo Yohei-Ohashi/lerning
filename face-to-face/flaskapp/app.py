d5648a1941335674a1d44e4dc662da41e7a206ab
Yohei Ohashi <hashi.life@outlook.jp>
2025-12-17 01:03:27 +0900

    feat: 会話履歴の保持

--- face-to-face/flaskapp/app.py
+++ face-to-face/flaskapp/app.py
@@ -12,6 +12,7 @@ client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 # Flaskアプリの作成
 app = Flask(__name__)
+chat_history = []
 
 
 # トップページへのアクセス設定 (ルートURLの定義)
@@ -22,13 +23,18 @@ def index():
     if request.method == "POST":
         # ユーザー入力
         user_input = request.form["user_input"]
-        # OpenAIに質問を投げる
+        # ユーザーの発言を履歴に追加
+        chat_history.append({"role": "user", "content": user_input})
+        
+        # 履歴ごとOpenAIに質問を投げる
         response = client.chat.completions.create(
-            model="gpt-4o", messages=[{"role": "user", "content": user_input}]
+            model="gpt-4o", messages=chat_history
         )
 
         # AIの回答
         bot_response = response.choices[0].message.content
+        # AIの回答も履歴に追加
+        chat_history.append({"role": "assistant", "content": bot_response})
 
         # 取得した値をresponseへ返す
         return render_template(
